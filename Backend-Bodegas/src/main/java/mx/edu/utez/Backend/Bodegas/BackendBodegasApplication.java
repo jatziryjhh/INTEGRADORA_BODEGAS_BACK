8ae3fc4b31257a3255ee6d1c0aa5b72f880d0d9a
HerreraHernandezJovannaJatziry <20223tn062@utez.edu.mx>
2025-02-27 10:29:51 -0600

    Proyecto Spring Boot

---
+++ Backend-Bodegas/src/main/java/mx/edu/utez/Backend/Bodegas/BackendBodegasApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.Backend.Bodegas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBodegasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBodegasApplication.class, args);
+	}
+
+}
---
+++ Backend-Bodegas/src/test/java/mx/edu/utez/Backend/Bodegas/BackendBodegasApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.utez.Backend.Bodegas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendBodegasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
